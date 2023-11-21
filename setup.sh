7d86ba3ffc7093b9e91986e00814e5630fba224c
Sam Der <sder@uci.edu>
2023-11-21 19:55:13 +0000

    first commit

---
+++ setup.sh
@@ -0,0 +1,18 @@
+# Install Docker
+## Add Docker's official GPG key:
+sudo apt update && sudo apt upgrade -y
+sudo apt install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+## Add the repository to Apt sources:
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin git python3 python3-pip python3-venv -y
+
+# Set up ROS docker container
+docker compose up -d
---
+++ talker.Dockerfile
@@ -0,0 +1,12 @@
+FROM ros:iron
+
+# install ros package
+
+RUN apt-get update && apt-get install -y \
+      ros-${ROS_DISTRO}-demo-nodes-cpp \
+      ros-${ROS_DISTRO}-demo-nodes-py && \
+    rm -rf /var/lib/apt/lists/*
+
+# launch ros package
+
+CMD ["ros2", "launch", "demo_nodes_cpp", "talker"]
